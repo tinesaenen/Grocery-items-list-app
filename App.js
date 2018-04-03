72ad3e5379804399e2294cf13a2b8cb10fd188e0
tine <tinesaenen@hotmail.be>
2018-04-03 11:07:54 +0200

    nieuwe tabbaricons geïnstalleerd

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import React from "react";
-import { Text, View, FlatList, TextInput } from "react-native";
+import { Text, View, FlatList, TextInput, Image } from "react-native";
 import { TabNavigator } from "react-navigation";
 import styles from "./Styles";
 
 import NewListScreen from "./NewListScreen";
 import AllListsScreen from "./AllListsScreen";
 
-export default TabNavigator({
-  NewList: { screen: NewListScreen },
-  AllLists: { screen: AllListsScreen }
-});
+export const newListIcon = require("./img/newListIcon2-02.png");
+export const allListsIcon = require("./img/allListsIcon-03.png");
+
+export default TabNavigator(
+  {
+    NewList: {
+      screen: NewListScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            style={[styles.tabbarIcon, { tintColor }]}
+            source={newListIcon}
+          />
+        )
+      }
+    },
+    AllLists: {
+      screen: AllListsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            style={[styles.tabbarIcon, { tintColor }]}
+            source={allListsIcon}
+          />
+        )
+      }
+    }
+  },
+  {
+    tabBarPosition: "bottom",
+    animationEnabled: true,
+    tabBarOptions: {
+      showLabel: true,
+      activeTintColor: "#FA7D7D",
+      inactiveTintColor: "#CECDCD",
+      style: {
+        backgroundColor: "white",
+        paddingBottom: 2
+      },
+      labelStyle: {
+        fontSize: 10
+      }
+    }
+  }
+);
