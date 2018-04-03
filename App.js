6755278d00dcf77ce64fdaafd0cc5243a3a54757
tine <tinesaenen@hotmail.be>
2018-04-03 11:43:16 +0200

    Tabnavigator in tabnavigator

--- App.js
+++ App.js
@@ -5,14 +5,62 @@ import styles from "./Styles";
 
 import NewListScreen from "./NewListScreen";
 import AllListsScreen from "./AllListsScreen";
+import TestLinks from "./testlinks";
+import TestRechts from "./testrechts";
 
 export const newListIcon = require("./img/newListIcon2-02.png");
 export const allListsIcon = require("./img/allListsIcon-03.png");
 
-export default TabNavigator(
+// export default TabNavigator
+const NewListOldItemNavigator = TabNavigator(
+  {
+    links: {
+      screen: NewListScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            style={[styles.tabbarIcon, { tintColor }]}
+            source={newListIcon}
+          />
+        )
+      }
+    },
+    rechts: {
+      screen: TestRechts,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            style={[styles.tabbarIcon, { tintColor }]}
+            source={allListsIcon}
+          />
+        )
+      }
+    }
+  },
+  {
+    tabBarPosition: "top",
+    animationEnabled: true,
+    swipeEnabled: true,
+
+    tabBarOptions: {
+      showLabel: true,
+      activeTintColor: "#FA7D7D",
+      inactiveTintColor: "#CECDCD",
+      style: {
+        backgroundColor: "white",
+        paddingBottom: 2
+      },
+      labelStyle: {
+        fontSize: 10
+      }
+    }
+  }
+);
+
+const MainTabNavigator = TabNavigator(
   {
     NewList: {
-      screen: NewListScreen,
+      screen: NewListOldItemNavigator,
       navigationOptions: {
         tabBarIcon: ({ tintColor }) => (
           <Image
@@ -51,3 +99,5 @@ export default TabNavigator(
     }
   }
 );
+
+export default MainTabNavigator;
---
+++ testrechts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Switch } from "react-native";
+import styles from "./Styles";
+
+export default class TestRechts extends React.Component {
+  render() {
+    return (
+      <View style={styles.item}>
+        <Text>hello rechts</Text>
+      </View>
+    );
+  }
+}
